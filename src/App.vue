46e7c7d4a4d762c17494b08abf86de2254ec4f33
Kamil Kerimov <kamil.kerimov@onlyoffice.com>
2023-12-22 22:44:37 +0500

    implemenet type trainer

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,124 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+import axios from "axios";
+import { ref, computed } from "vue";
+
+const text = ref('');
+const enteredText = ref('');
+
+const computedText = computed(() => {
+	return text.value.split('');
+})
+
+const gameOver = computed(() => {
+	return enteredText.value.length >= text.value.length;
+})
+
+const highlightedText = computed(() => {
+	return computedText.value.map((char, index) => { 
+		if(index > enteredText.value.length - 1)
+			return char;
+		const enteredChar = enteredText.value[index];
+		return char === enteredChar ? wrapCorrect(char) : wrapIncorrect(char);
+	})
+})
+
+const decomputedText = computed(() => {
+	return highlightedText.value.join('');
+})
+
+const getText = async () => {
+	const req = `https://fish-text.ru/get`;
+
+	const { data } = await axios(req);
+	text.value = data.text;
+}
+
+const wrapCorrect = (str: string) => {
+	return `<span class="correct">${str}</span>`;
+}
+
+const wrapIncorrect = (str: string) => {
+	return `<span class="incorrect">${str}</span>`;
+}
+
+const restart = () => {
+	getText();
+	enteredText.value = "";
+}
+
+const keyPress = (event: any) => {
+	const c = event.key;
+	enteredText.value = enteredText.value.concat(c);
+}
+
+getText();
+document.addEventListener("keypress", keyPress);
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+	<main>
+		<span v-html="decomputedText"></span>
+		<button v-if="gameOver" @click="restart()">Restart</button>
+	</main>
+	
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+
+main {
+	height: 100vh;
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	display: flex;
+	flex-direction: column;
+	gap: 5rem;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+span {
+	max-width: 1280px;
+	font-size: large;
+	color: whitesmoke;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+/* CSS */
+button {
+  --c: #fff;
+  /* text color */
+  background: linear-gradient(90deg, #0000 33%, #fff5, #0000 67%) var(--_p,100%)/300% no-repeat,
+    #004dff;
+  /* background color */
+  color: #0000;
+  border: none;
+  transform: perspective(500px) rotateY(calc(20deg*var(--_i,-1)));
+  text-shadow: calc(var(--_i,-1)* 0.08em) -.01em 0   var(--c),
+    calc(var(--_i,-1)*-0.08em)  .01em 2px #0004;
+  outline-offset: .1em;
+  transition: 0.3s;
 }
+
+button:hover,
+button:focus-visible {
+  --_p: 0%;
+  --_i: 1;
+}
+
+button:active {
+  text-shadow: none;
+  color: var(--c);
+  box-shadow: inset 0 0 9e9q #0005;
+  transition: 0s;
+}
+
+button {
+  font-weight: bold;
+  font-size: 2rem;
+  margin: 0;
+  cursor: pointer;
+  padding: .1em .3em;
+}
+
 </style>
